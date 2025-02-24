118015f12ac81239b733391a534d0d5747d5db60
vargas023 <alejandravargasb1@gmail.com>
2025-02-24 11:55:17 -0500

    avances boton guardar

--- proyecto_gadder/src/main.jsx
+++ proyecto_gadder/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Boton_guardar from './component/publics/boton_guardar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Boton_guardar/>
   </StrictMode>,
 )
